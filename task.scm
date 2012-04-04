1f6c091a4d4ebe0ff4c7e11172217c4975bc74d2
omegatakuma <tooth0819@gmail.com>
2012-04-04 20:27:12 +0900

    Update task.scm

--- task.scm
+++ task.scm
@@ -7,8 +7,8 @@
 
 (define clear
   (let1 c (process-output->string '("clear"))
-    (lambda ()
-      (display c))))
+		(lambda ()
+		  (display c))))
 (define (ToDo)
   (display "?> ")
   (flush)
@@ -21,7 +21,9 @@
 					  (print "***ERROR***\ncommand not found: " cmd)
 					  (ToDo))))))
 (define (w)
-  (let1 words (with-input-from-file (string-append (home-directory) "/SchemeToDo/.todo") (pa$ read))
+  (let1 words (with-input-from-file 
+				(string-append (home-directory) "/SchemeToDo/.todo") 
+				(pa$ read))
 		(if (eof-object? words)
 		  (writ (create) '())
 		  (writ (create) (cons words '())))))
@@ -39,18 +41,32 @@
 					(r)
 					(cons solve (cons ttime '()))))))))
 (define (writ result words)
-  (with-output-to-file (string-append (home-directory) "/SchemeToDo/.todo")
-					   (pa$ print
-							(if (null? words)
-								(x->string (cons result words))
-								(x->string (cons result (car words))))))
+  (with-output-to-file 
+	(string-append (home-directory) "/SchemeToDo/.todo")
+	(pa$ print
+		 (if (null? words)
+		   (x->string (cons result words))
+		   (x->string (cons result (car words))))))
   (w))
 (define (timer result)
-  (let*	((date1 (make-date 0 0 0 0 (date-day(current-date)) (date-month(current-date))  (date-year(current-date)) (date-zone-offset (current-date))))
-		 (date2 (make-date 0 0 0 0 (third result) (second result) (first result) (date-zone-offset (current-date)))))
+  (let*	((date1 
+		   (make-date 0 0 0 0 
+					  (date-day(current-date)) 
+					  (date-month(current-date))
+					  (date-year(current-date)) 
+					  (date-zone-offset (current-date))))
+		 (date2 (make-date 0 0 0 0 
+						   (third result) 
+						   (second result) 
+						   (first result) 
+						   (date-zone-offset (current-date)))))
 		(cond ((> (date->modified-julian-day date1) (date->modified-julian-day date2))'期限過ぎてます。)
 			  ((eqv? (date->modified-julian-day date1) (date->modified-julian-day date2))'期限日です。)
-			  (else (string->symbol (string-append "残り" (x->string (- (date->modified-julian-day date2) (date->modified-julian-day date1))) "日です。" ))))))
+			  (else (string->symbol
+					  (string-append "残り" 
+									 (x->string 
+									   (- (date->modified-julian-day date2) 
+										  (date->modified-julian-day date1)))"日です。"))))))
 (define (r)
   (let1 words (with-input-from-file (string-append (home-directory) "/SchemeTodo/.todo") (pa$ read))
 		(cond ((eof-object? words)
@@ -81,8 +97,8 @@
 											 (flush)
 											 (let1 ans (read)
 												   (if (eq? ans 'y)
-													   (x->string (filter symbol? words)))
-													 '())))
+													 (x->string (filter symbol? words)))
+												   '())))
 										  (else (string-append "***ERROR command not found: "n)))))
 			  (ToDo))))
 (define (main args)
